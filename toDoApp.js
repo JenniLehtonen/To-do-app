c1b6f27adb03216b7eae757c02dabecd04f6396b
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-01-02 14:02:02 +0200

    Add local storage

--- toDoApp.js
+++ toDoApp.js
@@ -8,7 +8,22 @@ input.addEventListener("keyup", function(event) {
 
 $(document).ready(function(){
     let newListItem = $("#newListItem");
-  
+    
+    
+    for (i = 0; i < localStorage.length; i++) {
+        x = localStorage.key(i);
+        let newLi1 = $("<li></li>");
+        let deleteButton1 = $("<button class='delete'>x</button>");
+        //$("#list").append("<li></li>"+localStorage.getItem(x)).append("<button class='delete'>x</button>");
+       //$("#list").append("<li></li>").append(localStorage.getItem(x)).append("<button class='delete'>x</button>");
+        newLi1.append(localStorage.getItem(x)).append(deleteButton1).append("<br>");
+        $("#list").append(newLi1);
+        
+    
+        console.log(newLi1);
+      }
+      
+
   $("#add").on( "click", function(){
     let valueToBeAdded = createNewLi(newListItem.val());
     
@@ -22,13 +37,16 @@ $(document).ready(function(){
   
     let newLi = $("<li></li>");
     let deleteButton = $("<button class='delete'>x</button>");
-  
+
     newLi.append(newListItem).append(deleteButton);
-  
+    randomId= "toDO" + Math.floor(Math.random() * 100) + 1;
+    localStorage.setItem(randomId, newListItem);
     return newLi;
   }
   
   $("#list").on("click", ".delete", function(){
-    $(this).parent().remove();  
+    $(this).parent().remove();
+    var testi=localStorage.key(this);
+    localStorage.removeItem(testi);  
   })
   });
